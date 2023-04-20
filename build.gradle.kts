922649225f3ea4f5673eeea8623d05d66c682db4
Maeda Naoki <maeda.naoki.md9@gmail.com>
2023-04-21 01:15:36 +0900

    build: update Kotlin plugins for Gradle ver1.8.20
    Ref : https://plugins.gradle.org/plugin/org.jetbrains.kotlin.android

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 plugins {
 	id("com.android.application") version "8.0.0" apply false
 	id("com.android.library") version "8.0.0" apply false
-	id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+	id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 	id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
 }
 
