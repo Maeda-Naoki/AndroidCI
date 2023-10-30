5032d5c19335291d4dde8d1743c95b194825f770
Naoki Maeda <maeda.naoki.md9@gmail.com>
2023-10-30 22:02:08 +0000

    chore(deps): update plugin org.jetbrains.kotlin.android to v1.9.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 plugins {
 	id("com.android.application") version "8.1.2" apply false
 	id("com.android.library") version "8.1.2" apply false
-	id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+	id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 	id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
 }
 
