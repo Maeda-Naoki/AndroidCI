0becfc20a5262df54f5178587b7d43b5b9d6560f
Naoki Maeda <maeda.naoki.md9@gmail.com>
2023-12-11 22:02:27 +0000

    chore(deps): update plugin org.jlleitschuh.gradle.ktlint to v12.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	id("com.android.application") version "8.2.0" apply false
 	id("com.android.library") version "8.2.0" apply false
 	id("org.jetbrains.kotlin.android") version "1.9.21" apply false
-	id("org.jlleitschuh.gradle.ktlint") version "12.0.2"
+	id("org.jlleitschuh.gradle.ktlint") version "12.0.3"
 }
 
 subprojects {
