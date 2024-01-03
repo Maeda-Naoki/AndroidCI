deec3da10a35604cfcf275f1bbcf16f174ae4693
Naoki Maeda <maeda.naoki.md9@gmail.com>
2024-01-03 22:03:10 +0000

    chore(deps): update android gradle plugin to v8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-	id("com.android.application") version "8.2.0" apply false
-	id("com.android.library") version "8.2.0" apply false
+	id("com.android.application") version "8.2.1" apply false
+	id("com.android.library") version "8.2.1" apply false
 	id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 	id("org.jlleitschuh.gradle.ktlint") version "12.0.3"
 }
