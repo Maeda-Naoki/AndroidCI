0a2215719517289d3d847d28bc74b71a51b24e18
Maeda Naoki <maeda.naoki.md9@gmail.com>
2024-01-13 18:12:35 +0900

    chore: migrate Gradle plugin specification method to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-	id("com.android.application") version "8.2.1" apply false
-	id("com.android.library") version "8.2.1" apply false
-	id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-	id("org.jlleitschuh.gradle.ktlint") version "12.0.3"
+	alias(libs.plugins.androidApplication) apply false
+	alias(libs.plugins.androidLibrary) apply false
+	alias(libs.plugins.kotlinAndroid) apply false
+	alias(libs.plugins.ktlint)
 }
 
 subprojects {
