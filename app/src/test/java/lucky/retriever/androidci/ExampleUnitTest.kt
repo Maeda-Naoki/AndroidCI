956cb057377cb49008cdb7640d345066ee50fc37
Maeda Naoki <maeda.naoki.md9@gmail.com>
2024-06-30 10:58:19 +0900

    test: unit test migrat from Junit to kotest

--- app/src/test/java/lucky/retriever/androidci/ExampleUnitTest.kt
+++ app/src/test/java/lucky/retriever/androidci/ExampleUnitTest.kt
@@ -1,16 +1,15 @@
 package lucky.retriever.androidci
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+class ExampleUnitTest : FunSpec({
+    test("addition_isCorrect") {
+        2 + 2 shouldBe 4
     }
-}
+})
