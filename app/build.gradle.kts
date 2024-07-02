33662fb87204c146696510834262671a2c1c2495
Maeda Naoki <maeda.naoki.md9@gmail.com>
2024-07-03 02:21:36 +0900

    build: update Build tools version in build.gradle to 35.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 		targetSdk = 34
 		versionCode = 1
 		versionName = "1.0"
-		buildToolsVersion = "34.0.0"
+		buildToolsVersion = "35.0.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
