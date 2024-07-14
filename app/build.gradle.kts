6f7d86338bd0f37882ad63ece198e9ce5ceb0c38
Maeda Naoki <maeda.naoki.md9@gmail.com>
2024-07-14 17:18:19 +0900

    build: enable code compression
    Ref: https://developer.android.com/build/shrink-code#shrink-code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 		}
 
 		getByName("release") {
-			isMinifyEnabled = false
+			isMinifyEnabled = true
 			proguardFiles(
 				getDefaultProguardFile("proguard-android-optimize.txt"),
 				"proguard-rules.pro",
